0c90d7d055ef057200cf362e2353b888aef85360
Sanansadig <sanan.sadig@hotmail.com>
2017-03-13 18:49:32 +0400

    Accordian

---
+++ accord.js
@@ -0,0 +1,43 @@
+
+function plusa() {
+	var x = document.getElementsByClassName("accord51");
+	 for (i=2; i<i+1; i++) {
+		if (i%2==0) { 
+		 x.style.display = "block"
+
+		} else {x.style.display = "none"}
+	}
+}
+
+function plusb() {
+	var x = document.getElementsByClassName("accord52");
+	for (i=2; i<i+1; i++) {
+		if (i%2==0) { 
+		 x.style.display = "block"
+
+		} else {x.style.display = "none"}
+	}
+
+}
+	
+
+function plusc() {
+	var x = document.getElementsByClassName("accord53");
+	 for (i=2; i<i+1; i++) {
+		if (i%2==0) { 
+		 x.style.display = "block"
+
+		} else {x.style.display = "none"}
+	}
+}
+
+function plusd() {
+	var x = document.getElementsByClassName("accord54");
+	 for (i=2; i<i+1; i++) {
+		if (i%2==0) { 
+		 x.style.display = "block"
+
+		} else {x.style.display = "none"}
+	}
+}
+		
